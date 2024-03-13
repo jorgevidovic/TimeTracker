532143bb05bdb11b77bc8b479b0cc1bac8553081
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2024-03-13 11:03:26 +0100

    Update

--- app/src/main/java/com/vidovicsystems/timetracker/data/room/ChronosDatabaseDao.kt
+++ app/src/main/java/com/vidovicsystems/timetracker/data/room/ChronosDatabaseDao.kt
@@ -7,7 +7,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.vidovicsystems.timetracker.data.Chronos
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
 
 // Interface -> Repository -> Viewmodel -> View
 
